1a89bf309cf5f4073d3d6343523978110960ebf7
Maria Genis <magegr@alumnatflorida.es>
2025-11-04 11:21:33 +0100

    Añadido resultado.php

---
+++ resultado.php
@@ -0,0 +1,16 @@
+<?php
+echo "La partida a terminado"
+?>
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Mastermind Numérico</title>
+</head>
+<body>
+<form>
+<label>
+ <input type="submit" value="Enviar">
+</label>
+</form>
+</body>
